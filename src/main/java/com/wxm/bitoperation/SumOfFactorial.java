ec7b772d43e418e4198b67dd5ff52e8cccebf866
wxm <510600441@qq.com>
2022-02-08 18:03:35 +0800

    代码提交

---
+++ src/main/java/com/wxm/bitoperation/SumOfFactorial.java
@@ -0,0 +1,29 @@
+package com.wxm.bitoperation;
+
+/**
+ * @author: wxm
+ * @created: 2022/02/08
+ */
+public class SumOfFactorial {
+
+    /**
+     *  给定一个参数N，求 1! + 2！+ 3！+...+ N! 的值
+     */
+
+    public static void main(String[] args){
+
+        int n = 4;
+        System.out.println(sumOfFactorial(n));
+
+    }
+
+    public static int sumOfFactorial(int n){
+        int temp = 1;
+        int result = 0;
+        for(int i = 1; i <= n; i++){
+            temp = temp * i;
+            result += temp;
+        }
+        return result;
+    }
+}

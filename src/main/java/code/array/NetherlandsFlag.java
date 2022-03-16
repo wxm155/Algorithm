4fdaee5c56e03d24ad8300cd74541e15b88eae05
wxm <510600441@qq.com>
2022-03-16 14:47:42 +0800

    添加荷兰国旗问题

--- src/main/java/code/array/NetherlandsFlag.java
+++ src/main/java/code/array/NetherlandsFlag.java
@@ -11,30 +11,30 @@ public class NetherlandsFlag {
      * 将数组划分为 小于arr[arr.length -1]、等于arr[arr.length -1]、大于arr[arr.length -1],可以无序
      */
 
-    public static void netherlandsFlag(int[] arr){
-        if (arr == null || arr.length < 2){
+    public static void netherlandsFlag(int[] arr) {
+        if (arr == null || arr.length < 2) {
             return;
         }
         int left = -1;
-        int right = arr.length -1;
+        int right = arr.length - 1;
         int index = 0;
-        int num = arr[arr.length -1];
+        int num = arr[arr.length - 1];
         // 左边指针和右边指针分别向中间移动
-        while (index < right){
-            if (arr[index] < num){
-                swap(arr,++left,index++);
-            }else if (arr[index] == num){
+        while (index < right) {
+            if (arr[index] < num) {
+                swap(arr, ++left, index++);
+            } else if (arr[index] == num) {
                 index++;
-            }else {
-                swap(arr,index,--right);
+            } else {
+                swap(arr, index, --right);
             }
         }
         // 交换划分值的位置
-        swap(arr,right,arr.length -1);
+        swap(arr, right, arr.length - 1);
 
     }
 
-    public static void swap(int[] arr,int i,int j){
+    public static void swap(int[] arr, int i, int j) {
         int temp = arr[i];
         arr[i] = arr[j];
         arr[j] = temp;

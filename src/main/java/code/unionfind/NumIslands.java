9e0b2b389ea662cd629c3d04a50a02987ade52b1
wxm <510600441@qq.com>
2022-07-15 09:37:23 +0800

    添加岛的数量并查集解法

--- src/main/java/code/unionfind/NumIslands.java
+++ src/main/java/code/unionfind/NumIslands.java
@@ -57,4 +57,107 @@ public class NumIslands {
         infect(grid, i, j - 1);
         infect(grid, i, j + 1);
     }
+
+
+    /**
+     * 并查集解法：只须判断节点的左边和上边是否需要合并
+     * @param grid
+     * @return
+     */
+    public int numIslands2(char[][] grid) {
+
+        int len = grid.length;
+        int col = grid[0].length;
+        UnionFind unionFind = new UnionFind(grid);
+        // 左边界处理
+        for (int i = 1; i < len; i++) {
+            if (grid[i - 1][0] == '1' && grid[i][0] == '1') {
+                unionFind.union(i - 1, 0, i, 0);
+            }
+        }
+        // 上边界处理
+        for (int i = 1; i < col; i++) {
+            if (grid[0][i - 1] == '1' && grid[0][i] == '1') {
+                unionFind.union(0, i - 1, 0, i);
+            }
+        }
+        // 剩余位置
+        for (int i = 1; i < len; i++) {
+            for (int j = 1; j < col; j++) {
+                if (grid[i - 1][j] == '1' && grid[i][j] == '1') {
+                    unionFind.union(i - 1, j, i, j);
+                }
+                if (grid[i][j - 1] == '1' && grid[i][j] == '1') {
+                    unionFind.union(i, j - 1, i, j);
+                }
+            }
+        }
+        return unionFind.sets;
+
+    }
+
+    /**
+     * 并查集
+     */
+    public class UnionFind {
+        private int[] parent;
+        private int[] size;
+        private int[] help;
+        private int sets;
+        private int col;
+
+        public UnionFind(char[][] grid) {
+            int len = grid.length;
+            col = grid[0].length;
+            int num = len * col;
+            parent = new int[num];
+            size = new int[num];
+            help = new int[num];
+            sets = 0;
+            for (int i = 0; i < len; i++) {
+                for (int j = 0; j < col; j++) {
+                    if (grid[i][j] == '1') {
+                        int index = index(i, j);
+                        parent[index] = index;
+                        size[index] = 1;
+                        sets++;
+                    }
+                }
+            }
+        }
+        // 计算二维数组中坐标对应一维数组位置
+        public int index(int i, int j) {
+            return col * i + j;
+        }
+
+        public int find(int i) {
+            int temp = 0;
+            while (i != parent[i]) {
+                help[temp++] = i;
+                i = parent[i];
+            }
+            // 路径压缩
+            for (int j = 0; j < temp; j++) {
+                parent[help[j]] = i;
+            }
+            return i;
+        }
+
+        public void union(int i1, int j1, int i2, int j2) {
+            int r1 = index(i1, j1);
+            int r2 = index(i2, j2);
+            int f1 = find(r1);
+            int f2 = find(r2);
+            if (f1 != f2) {
+                if (size[f1] > size[f2]) {
+                    size[f1] += size[f2];
+                    parent[f2] = f1;
+                } else {
+                    size[f2] += size[f1];
+                    parent[f1] = f2;
+                }
+                sets--;
+            }
+        }
+    }
 }

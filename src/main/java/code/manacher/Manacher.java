c4cfe8873a46ee11a6fbf79167f7347276ffc1b7
wxm <510600441@qq.com>
2022-09-24 10:48:31 +0800

    添加Manacher算法

--- src/main/java/code/manacher/Manacher.java
+++ src/main/java/code/manacher/Manacher.java
@@ -11,12 +11,34 @@ public class Manacher {
      */
 
     public static int manacher(String str) {
-        if (str == null || str.length() == 0){
+        if (str == null || str.length() == 0) {
             return 0;
         }
         char[] strArr = manacherStr(str);
-        // TODO....
-        return 0;
+        int[] pArr = new int[strArr.length];
+        int c = -1, r = -1;
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < strArr.length; i++) {
+            // 2 * c - i 为i对称的位置
+            pArr[i] = r > i ? Math.min(pArr[2 * c - i], r - i) : 1;
+            while (i + pArr[i] < strArr.length && i - pArr[i] > -1) {
+                if (strArr[i + pArr[i]] == strArr[i - pArr[i]]) {
+                    pArr[i]++;
+                } else {
+                    break;
+                }
+            }
+            // r扩的更远
+            int temp = i + pArr[i];
+            if (temp > r) {
+                r = temp;
+                c = i;
+            }
+            max = Math.max(max, pArr[i]);
+        }
+        // 121 => #1#2#1#  回文半径为4
+        // 1221 => #1#2#2#1#  回文半径为5
+        return max - 1;
     }
 
 
@@ -26,9 +48,51 @@ public class Manacher {
         char[] chars = str.toCharArray();
         char[] res = new char[str.length() * 2 + 1];
         int index = 0;
-        for (int i = 0; i < res.length; i++) {
+        for (int i = 0; i != res.length; i++) {
             res[i] = (i & 1) == 0 ? '#' : chars[index++];
         }
         return res;
+    }
+
+    // for test
+    public static int right(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+        char[] str = manacherStr(s);
+        int max = 0;
+        for (int i = 0; i < str.length; i++) {
+            int L = i - 1;
+            int R = i + 1;
+            while (L >= 0 && R < str.length && str[L] == str[R]) {
+                L--;
+                R++;
+            }
+            max = Math.max(max, R - L - 1);
+        }
+        return max / 2;
+    }
+
+    // for test
+    public static String getRandomString(int possibilities, int size) {
+        char[] ans = new char[(int) (Math.random() * size) + 1];
+        for (int i = 0; i < ans.length; i++) {
+            ans[i] = (char) ((int) (Math.random() * possibilities) + 'a');
+        }
+        return String.valueOf(ans);
+    }
+
+    public static void main(String[] args) {
+        int possibilities = 5;
+        int strSize = 20;
+        int testTimes = 5000000;
+        System.out.println("test begin");
+        for (int i = 0; i < testTimes; i++) {
+            String str = getRandomString(possibilities, strSize);
+            if (manacher(str) != right(str)) {
+                System.out.println("fuck.....");
+            }
+        }
+        System.out.println("test finish");
     }
 }

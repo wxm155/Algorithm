5c1183a58058c043479dc9b8e4f5c655e368dd28
wxm <510600441@qq.com>
2022-12-02 14:17:11 +0800

    添加最大正方形面积和最少的船的次数

---
+++ src/main/java/code/highFrequency/BoatsToSavePeople.java
@@ -0,0 +1,19 @@
+package code.highFrequency;
+
+/**
+ * @author: wxm
+ * @created: 2022/12/02
+ */
+public class BoatsToSavePeople {
+
+    /**
+     * 给定一个正数数组arr，代表若干人的体重，再给定一个正数limit，表示所有船共同拥有的载重量，
+     * 每艘船最多坐两人，且不能超过载重 想让所有的人同时过河，
+     * 并且用最好的分配方法让船尽量少，返回最少的船数
+     */
+
+    public int numRescueBoats(int[] people, int limit) {
+
+        return 0;
+    }
+}
---
+++ src/main/java/code/highFrequency/Largest1BorderedSquare.java
@@ -0,0 +1,31 @@
+package code.highFrequency;
+
+/**
+ * @author: wxm
+ * @created: 2022/12/02
+ */
+public class Largest1BorderedSquare {
+
+    /**
+     * 给你一个由若干 0 和 1 组成的二维网格 grid，请你找出边界全部由 1 组成的最大正方形子网格，并返回该子网格中的元素数量。如果不存在，则返回 0。
+     *
+     * 示例 1：
+     * 输入：grid = [[1,1,1],[1,0,1],[1,1,1]]
+     * 输出：9
+     *
+     * 示例 2：
+     * 输入：grid = [[1,1,0,0]]
+     * 输出：1
+     *
+     * 提示：
+     * 1 <= grid.length <= 100
+     * 1 <= grid[0].length <= 100
+     * grid[i][j] 为 0 或 1
+     *
+     * 力扣：https://leetcode.cn/problems/largest-1-bordered-square/
+     */
+
+    public int largest1BorderedSquare(int[][] grid) {
+        return 0;
+    }
+}

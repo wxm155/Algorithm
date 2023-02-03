639cdfa2d75112115e027d41d474f5d32a99f490
wxm <510600441@qq.com>
2023-02-03 18:43:06 +0800

    添加不能相邻的子序列最大累加和空间压缩优化

--- src/main/java/code/dynamicProgramming/SubArrayMaxSumFollowUp.java
+++ src/main/java/code/dynamicProgramming/SubArrayMaxSumFollowUp.java
@@ -58,4 +58,25 @@ public class SubArrayMaxSumFollowUp {
         }
         return dp[nums.length - 1];
     }
+
+    /**
+     * 空间压缩优化
+     * @param nums 输入数组
+     * @return result
+     */
+    public static int rob2(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        if (nums.length == 1) {
+            return nums[0];
+        }
+        int p1 = nums[0], p2 = Math.max(nums[0], nums[1]);
+        for (int i = 2; i < nums.length; i++) {
+            int tem = p2;
+            p2 = Math.max(p1 + nums[i], Math.max(p2, nums[i]));
+            p1 = tem;
+        }
+        return p2;
+    }
 }

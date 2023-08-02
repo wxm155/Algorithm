0a5c66cca32d89b28362eb0141d2d7a59319a682
wxm <510600441@qq.com>
2023-08-02 16:10:31 +0800

    添加分割回文串回溯所有结果

--- src/main/java/code/dynamicProgramming/PalindromePartitioningII.java
+++ src/main/java/code/dynamicProgramming/PalindromePartitioningII.java
@@ -136,4 +136,123 @@ public class PalindromePartitioningII {
         }
         return ans;
     }
+
+    /**
+     * 在问题一的基础上返回所有划分结果
+     */
+    public static List<List<String>> minCutAllWays(String s) {
+        List<List<String>> ans = new ArrayList<>();
+        if (s == null || s.length() < 2) {
+            List<String> cur = new ArrayList<>();
+            cur.add(s);
+            ans.add(cur);
+        } else {
+            int N = s.length();
+            char[] str = s.toCharArray();
+            boolean[][] checkMap = createCheckMap(str, N);
+            // dp[i]表示str[i...N-1]范围上最少有多少个回文串
+            int[] dp = new int[N + 1];
+            // 从右往左填
+            for (int i = N - 1; i >= 0; i--) {
+                if (checkMap[i][N - 1]) {
+                    dp[i] = 1;
+                } else {
+                    int next = Integer.MAX_VALUE;
+                    // 依次求出最少的回文串个数
+                    for (int j = i; j < N; j++) {
+                        // 如果str[i...j]是回文串，则str[i...N-1]的回文串个数为dp[j + 1] + 1
+                        if (checkMap[i][j]) {
+                            next = Math.min(next, dp[j + 1]);
+                        }
+                    }
+                    dp[i] = next + 1;
+                }
+            }
+            process(s, 0, 1, checkMap, dp, new ArrayList<>(), ans);
+        }
+        return ans;
+    }
+
+    // str[i...j]上划分的回文串放在ans中
+    public static void process(String str, int i, int j, boolean[][] checkMap, int[] dp, List<String> path, List<List<String>> ans) {
+        if (j == str.length()) {
+            if (checkMap[i][j - 1] && dp[i] == dp[j] + 1) {
+                path.add(str.substring(i, j));
+                ans.add(new ArrayList<>(path));
+                // 深度优先遍历恢复现场
+                path.remove(path.size() - 1);
+            }
+        } else {
+            // 当前满足分割，继续深度优先遍历
+            if (checkMap[i][j - 1] && dp[i] == dp[j] + 1) {
+                path.add(str.substring(i, j));
+                process(str, j, j + 1, checkMap, dp, path, ans);
+                // 深度优先遍历恢复现场
+                path.remove(path.size() - 1);
+            }
+            // 不满足分割，继续深度优先遍历
+            process(str, i, j + 1, checkMap, dp, path, ans);
+        }
+    }
+
+    public static void main(String[] args) {
+        String s = null;
+        List<String> ans2 = null;
+        List<List<String>> ans3 = null;
+
+        System.out.println("本题第二问，返回其中一种结果测试开始");
+        s = "abacbc";
+        ans2 = minCutOneWay(s);
+        for (String str : ans2) {
+            System.out.print(str + " ");
+        }
+        System.out.println();
+
+        s = "aabccbac";
+        ans2 = minCutOneWay(s);
+        for (String str : ans2) {
+            System.out.print(str + " ");
+        }
+        System.out.println();
+
+        s = "aabaa";
+        ans2 = minCutOneWay(s);
+        for (String str : ans2) {
+            System.out.print(str + " ");
+        }
+        System.out.println();
+        System.out.println("本题第二问，返回其中一种结果测试结束");
+        System.out.println();
+        System.out.println("本题第三问，返回所有可能结果测试开始");
+        s = "cbbbcbc";
+        ans3 = minCutAllWays(s);
+        for (List<String> way : ans3) {
+            for (String str : way) {
+                System.out.print(str + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+        s = "aaaaaa";
+        ans3 = minCutAllWays(s);
+        for (List<String> way : ans3) {
+            for (String str : way) {
+                System.out.print(str + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+        s = "fcfffcffcc";
+        ans3 = minCutAllWays(s);
+        for (List<String> way : ans3) {
+            for (String str : way) {
+                System.out.print(str + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+        System.out.println("本题第三问，返回所有可能结果测试结束");
+    }
 }

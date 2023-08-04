44e163fe544cf8b30376eae1d8cfb4a52bcc2e58
wxm <510600441@qq.com>
2023-08-04 09:52:34 +0800

    添加让字符串成为回文串的最少插入次数通过动态规划表回溯结果

--- src/main/java/code/dynamicProgramming/MinimumInsertionStepsToMakeAStringPalindrome.java
+++ src/main/java/code/dynamicProgramming/MinimumInsertionStepsToMakeAStringPalindrome.java
@@ -1,5 +1,8 @@
 package code.dynamicProgramming;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author: wxm
  * @created: 2023/08/02
@@ -96,16 +99,16 @@ public class MinimumInsertionStepsToMakeAStringPalindrome {
         while (L < R) {
             // 情况一：是以结尾字符添加一个在开头
             if (dp[L][R] == dp[L][R - 1] + 1) {
-                ans[ansL] = str[R];
-                ans[ansR] = str[R--];
+                ans[ansL++] = str[R];
+                ans[ansR--] = str[R--];
                 // 情况二：是以开头字符添加一个在结尾
             } else if (dp[L][R] == dp[L + 1][R] + 1) {
-                ans[ansL] = str[L];
-                ans[ansR] = str[L++];
+                ans[ansL++] = str[L];
+                ans[ansR--] = str[L++];
                 // 情况三：开头字符和结尾字符相同
             } else {
-                ans[ansL] = str[L++];
-                ans[ansR] = str[R--];
+                ans[ansL++] = str[L++];
+                ans[ansR--] = str[R--];
             }
         }
         if (L == R) {
@@ -113,4 +116,102 @@ public class MinimumInsertionStepsToMakeAStringPalindrome {
         }
         return String.valueOf(ans);
     }
+
+    public static List<String> minInsertionsAllWays(String s) {
+        if (s == null || s.length() < 2) {
+            List<String> ans = new ArrayList<>();
+            ans.add(s);
+            return ans;
+        }
+        char[] str = s.toCharArray();
+        int len = s.length();
+        int[][] dp = new int[len][len];
+        for (int i = 0; i < len - 1; i++) {
+            dp[i][i + 1] = str[i] == str[i + 1] ? 0 : 1;
+        }
+        for (int i = len - 3; i >= 0; i--) {
+            for (int j = i + 2; j < len; j++) {
+                dp[i][j] = Math.min(dp[i + 1][j], dp[i][j - 1]) + 1;
+                if (str[i] == str[j]) {
+                    dp[i][j] = Math.min(dp[i][j], dp[i + 1][j - 1]);
+                }
+            }
+        }
+        char[] path = new char[len + dp[0][len - 1]];
+        List<String> ans = new ArrayList<>();
+        getAllWays(str, 0, len - 1, path, 0, path.length - 1, dp, ans);
+        return ans;
+    }
+
+    public static void getAllWays(char[] str, int L, int R, char[] path, int pl, int pr, int[][] dp, List<String> ans) {
+        if (L >= R) {
+            if (L == R) {
+                path[pl] = str[L];
+            }
+            ans.add(String.valueOf(path));
+        } else {
+            // 深度优先遍历不需要恢复现场，因为每次都会覆盖当前值
+            if (dp[L][R] == dp[L][R - 1] + 1) {
+                path[pl] = str[R];
+                path[pr] = str[R];
+                getAllWays(str, L, R - 1, path, pl + 1, pr - 1, dp, ans);
+            }
+            if (dp[L][R] == dp[L + 1][R] + 1) {
+                path[pl] = str[L];
+                path[pr] = str[L];
+                getAllWays(str, L + 1, R, path, pl + 1, pr - 1, dp, ans);
+            }
+            if (str[L] == str[R] && (L == R - 1 || dp[L + 1][R - 1] == dp[L][R])) {
+                path[pl] = str[L];
+                path[pr] = str[R];
+                getAllWays(str, L + 1, R - 1, path, pl + 1, pr - 1, dp, ans);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        String s = null;
+        String ans2 = null;
+        List<String> ans3 = null;
+
+        System.out.println("本题第二问，返回其中一种结果测试开始");
+        s = "mbadm";
+        ans2 = minInsertionsOneWay(s);
+        System.out.println(ans2);
+
+        s = "leetcode";
+        ans2 = minInsertionsOneWay(s);
+        System.out.println(ans2);
+
+        s = "aabaa";
+        ans2 = minInsertionsOneWay(s);
+        System.out.println(ans2);
+        System.out.println("本题第二问，返回其中一种结果测试结束");
+
+        System.out.println();
+
+        System.out.println("本题第三问，返回所有可能的结果测试开始");
+        s = "mbadm";
+        ans3 = minInsertionsAllWays(s);
+        for (String way : ans3) {
+            System.out.println(way);
+        }
+        System.out.println();
+
+        s = "leetcode";
+        ans3 = minInsertionsAllWays(s);
+        for (String way : ans3) {
+            System.out.println(way);
+        }
+        System.out.println();
+
+        s = "aabaa";
+        ans3 = minInsertionsAllWays(s);
+        for (String way : ans3) {
+            System.out.println(way);
+        }
+        System.out.println();
+        System.out.println("本题第三问，返回所有可能的结果测试结束");
+    }
+
 }

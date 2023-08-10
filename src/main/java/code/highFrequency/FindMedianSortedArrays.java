422d51840d9fb3612e8c24a23a5f6e76eef6e72f
wxm <510600441@qq.com>
2023-08-10 18:50:49 +0800

    添加查找两个正序数组的第K小的数

---
+++ src/main/java/code/highFrequency/FindMedianSortedArrays.java
@@ -0,0 +1,130 @@
+package code.highFrequency;
+
+/**
+ * @author: wxm
+ * @created: 2023/08/10
+ */
+public class FindMedianSortedArrays {
+
+    /**
+     * 给定两个大小分别为 m 和 n 的正序（从小到大）数组 nums1 和 nums2。请你找出并返回这两个正序数组的 中位数 。
+     * 算法的时间复杂度应该为 O(log (m+n)) 。
+     * <p>
+     * 示例 1：
+     * 输入：nums1 = [1,3], nums2 = [2]
+     * 输出：2.00000
+     * 解释：合并数组 = [1,2,3] ，中位数 2
+     * <p>
+     * 示例 2：
+     * 输入：nums1 = [1,2], nums2 = [3,4]
+     * 输出：2.50000
+     * 解释：合并数组 = [1,2,3,4] ，中位数 (2 + 3) / 2 = 2.5
+     * <p>
+     * 提示：
+     * nums1.length == m
+     * nums2.length == n
+     * 0 <= m <= 1000
+     * 0 <= n <= 1000
+     * 1 <= m + n <= 2000
+     * -10^6 <= nums1[i], nums2[i] <= 10^6
+     * <p>
+     * 力扣：https://leetcode.cn/problems/median-of-two-sorted-arrays/description/
+     * <p>
+     * 扩展题目：
+     * 给定两个大小分别为 m 和 n 的正序（从小到大）数组 nums1 和 nums2。请你找出并返回这两个正序数组的中位数
+     * 进阶：在两个都有序的数组中找整体第K小的数，可以做到O(log(Min(M,N)))
+     */
+
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        return 0.0;
+    }
+
+    // 查找两个正序数组的第K小的数
+    public static int findKthNum(int[] arr1, int[] arr2, int kth) {
+        int[] longs = arr1.length > arr2.length ? arr1 : arr2;
+        int[] shorts = arr1.length > arr2.length ? arr2 : arr1;
+        int l = longs.length, s = shorts.length;
+        // 1、k <= s
+        if (kth <= s) {
+            return getUpMedian(shorts, 0, kth - 1, longs, 0, kth - 1);
+        }
+        // 2、k > l
+        if (kth > l) {
+            // 判断一下，剔除多余数字让数组等长
+            if (shorts[kth - l - 1] >= longs[l - 1]) {
+                return shorts[kth - l - 1];
+            }
+            if (longs[kth - l - 1] >= shorts[s - 1]) {
+                return longs[kth - l - 1];
+            }
+            return getUpMedian(shorts, kth - l, s - 1, longs, kth - s, l - 1);
+        }
+        // 3、s < k <= l
+        // 判断一下，剔除多余数字让数组等长
+        if (longs[kth - s - 1] >= shorts[s - 1]) {
+            return longs[kth - s - 1];
+        }
+        // 2 5 10
+        return getUpMedian(shorts, 0, s - 1, longs, kth - s, kth - 1);
+    }
+
+    // A.length = B.length
+    // 获取两个数组的上中位数
+    // 数字仅代表次序
+    // 奇数例子：
+    // A: 1  2  3  4  5  6  7
+    // B: 1` 2` 3` 4` 5` 6` 7`
+    // 偶数例子：
+    // A: 1  2  3  4  5  6
+    // B: 1` 2` 3` 4` 5` 6`
+    // 核心思想：二分法
+    public static int getUpMedian(int[] A, int s1, int e1, int[] B, int s2, int e2) {
+        int midA, midB;
+        while (s1 < e1) {
+            midA = (s1 + e1) / 2;
+            midB = (s2 + e2) / 2;
+            if (A[midA] == B[midB]) {
+                return A[midA];
+            }
+            // 数组长度为奇数
+            if (((e1 - s1 + 1) & 1) == 1) {
+                // 4 > 4` 数组变为
+                // A: 1  2  3
+                // B: 5` 6` 7`
+                // 其余位置一定不是中位数
+                if (A[midA] > B[midB]) {
+                    if (B[midB] >= A[midA - 1]) {
+                        return B[midB];
+                    }
+                    e1 = midA - 1;
+                    s2 = midB + 1;
+                } else {
+                    // 4 < 4` 数组变为
+                    // A: 5  6  7
+                    // B: 1` 2` 3`
+                    if (A[midA] >= B[midB - 1]) {
+                        return A[midA];
+                    }
+                    e2 = midB - 1;
+                    s1 = midA + 1;
+                }
+                // 数组长度为偶数
+            } else {
+                // 数组变为：
+                // A: 1  2  3
+                // B: 4` 5` 6`
+                if (A[midA] > B[midB]) {
+                    e1 = midA;
+                    s2 = midB + 1;
+                } else {
+                    // 数组变为：
+                    // A: 4  5  6
+                    // B: 1` 2` 3`
+                    e2 = midB;
+                    s1 = midA + 1;
+                }
+            }
+        }
+        return Math.min(A[s1], B[s2]);
+    }
+}

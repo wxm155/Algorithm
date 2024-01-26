3bd5db3b9997809cb61e70a434d149f87616bb02
wxm <510600441@qq.com>
2024-01-26 16:56:50 +0800

    添加两数之和和字母异位词分组

---
+++ src/main/java/code/leetCodeTop100/GroupAnagrams.java
@@ -0,0 +1,49 @@
+package code.leetCodeTop100;
+
+import java.util.*;
+
+/**
+ * @author: wxm
+ * @created: 2024/01/26
+ */
+public class GroupAnagrams {
+
+    /**
+     * 给你一个字符串数组，请你将 字母异位词 组合在一起。可以按任意顺序返回结果列表。
+     * 字母异位词 是由重新排列源单词的所有字母得到的一个新单词。
+     * <p>
+     * 示例 1:
+     * 输入: strs = ["eat", "tea", "tan", "ate", "nat", "bat"]
+     * 输出: [["bat"],["nat","tan"],["ate","eat","tea"]]
+     * <p>
+     * 示例 2:
+     * 输入: strs = [""]
+     * 输出: [[""]]
+     * <p>
+     * 示例 3:
+     * 输入: strs = ["a"]
+     * 输出: [["a"]]
+     * <p>
+     * 提示：
+     * 1 <= strs.length <= 10^4
+     * 0 <= strs[i].length <= 100
+     * strs[i] 仅包含小写字母
+     */
+
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        for (String str : strs) {
+            char[] chars = str.toCharArray();
+            Arrays.sort(chars);
+            String sortStr = new String(chars);
+            if (map.containsKey(sortStr)) {
+                map.get(sortStr).add(str);
+            } else {
+                List<String> list = new ArrayList<>();
+                list.add(str);
+                map.put(sortStr, list);
+            }
+        }
+        return new ArrayList<>(map.values());
+    }
+}
---
+++ src/main/java/code/leetCodeTop100/TwoSum.java
@@ -0,0 +1,63 @@
+package code.leetCodeTop100;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author: wxm
+ * @created: 2024/01/26
+ */
+public class TwoSum {
+
+    /**
+     * 给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target  的那 两个 整数，并返回它们的数组下标。
+     * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现。
+     * 你可以按任意顺序返回答案。
+     * <p>
+     * 示例 1：
+     * 输入：nums = [2,7,11,15], target = 9
+     * 输出：[0,1]
+     * 解释：因为 nums[0] + nums[1] == 9 ，返回 [0, 1] 。
+     * <p>
+     * 示例 2：
+     * 输入：nums = [3,2,4], target = 6
+     * 输出：[1,2]
+     * <p>
+     * 示例 3：
+     * 输入：nums = [3,3], target = 6
+     * 输出：[0,1]
+     * <p>
+     * 提示：
+     * 2 <= nums.length <= 10^4
+     * -109 <= nums[i] <= 10^9
+     * -109 <= target <= 10^9
+     * 只会存在一个有效答案
+     * <p>
+     * <p>
+     * 进阶：你可以想出一个时间复杂度小于 O(n2) 的算法吗？
+     */
+
+    // O(n^2)
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return null;
+    }
+
+    // O(n)
+    public int[] twoSum2(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                return new int[]{map.get(target - nums[i]), i};
+            }
+            map.put(nums[i], i);
+        }
+        return null;
+    }
+}
